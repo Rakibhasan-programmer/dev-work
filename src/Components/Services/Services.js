11e98ace1cf98e6175ab55fcdcdfb5203186fca1
unknown <rakibulhasan.seu.cse@gmail.com>
2023-02-27 23:27:11 +0600

    slight changes in services page

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -170,7 +170,7 @@ const Services = () => {
                       </li>
                       <li>
                         <a
-                          href="#"
+                          href="/"
                           className="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                         >
                           Printing Paper
