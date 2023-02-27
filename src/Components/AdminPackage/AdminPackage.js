76f730b49bcefe00bc3808e441f9a92593c4d87b
unknown <rakibulhasan.seu.cse@gmail.com>
2023-02-27 20:51:40 +0600

    packages card color change

--- src/Components/AdminPackage/AdminPackage.js
+++ src/Components/AdminPackage/AdminPackage.js
@@ -3,12 +3,12 @@ import AdminCard from '../AdminCard/AdminCard';
 
 const AdminPackage = () => {
     const value = [
-        {name: "Bronze", mBalance: "501$-800$", id: 1, isActive: false},
-        {name: "Bronze", mBalance: "501$-800$", id: 2, isActive: true},
-        {name: "Silver", mBalance: "801$-1000$", id: 3, isActive: true},
-        {name: "Platinum", mBalance: "301$-500$", id: 4, isActive: true},
-        {name: "Gold", mBalance: "501$-800$", id: 5, isActive: true},
-        {name: "Diamond", mBalance: "801$-1000$", id: 6, isActive: true}
+        {name: "Bronze", mBalance: "501$-800$", id: 1, isActive: false, bgColor: '#C3BE47', bgColorBody: '#FDFBC4'},
+        {name: "Bronze", mBalance: "501$-800$", id: 2, isActive: true, bgColor: '#C3BE47', bgColorBody: '#FDFBC4'},
+        {name: "Silver", mBalance: "801$-1000$", id: 3, isActive: true, bgColor: '#FFFFFF', bgColorBody: '#DADADA'},
+        {name: "Platinum", mBalance: "301$-500$", id: 4, isActive: true, bgColor: '#8756EF', bgColorBody: '#E4D8FF'},
+        {name: "Gold", mBalance: "501$-800$", id: 5, isActive: true, bgColor: '#C3BE47', bgColorBody: '#FDFBC4'},
+        {name: "Diamond", mBalance: "801$-1000$", id: 6, isActive: true, bgColor: '#FFFFFF', bgColorBody: '#DADADA'}
     ];
     return (
         <div className='grid grid-row md:grid-cols-2 lg:grid-cols-3 gap-10 px-24 py-24'>
